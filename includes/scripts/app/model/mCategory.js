07b6cb30c15ff41e2c5dfefcab2774f9a70290c7
Arnau Monclús Sánchez <arnaunaui@gmail.com>
2016-10-06 13:02:43 +0200

    Added parameters to url for filtering

--- includes/scripts/app/model/mCategory.js
+++ includes/scripts/app/model/mCategory.js
@@ -39,24 +39,30 @@ define(['./Base', '../libCommon', 'bootstrap_slider', 'plugins'], function (Base
 
 	/**
 	 * Category page - Show active filter in top of filter box
-	 * @param option:String Selected option to show
-	 * @param option:String Option value as identifier
+	 * @param option: String Selected option to show
+	 * @param value: String Option value as identifier
+	 * @param type: Type of filter
 	 */
-	function enableFilter(option, value) {
-		
+	function enableFilter(option, value, type) {
+		var $resultados = $('.resultados');
 		$('#filtroInfo').hide();
-		if ( value == "price" && $('.resultados').find('.active-filter#price').length > 0 ) {
-			$('.resultados').find('#price span.text').remove();
-			$('.resultados').find('#price').append('<span class="text">'+option+'</span>');
-		} else {
-			$('.resultados').find('.active-filter:not([id])').first().attr('id', value);
-			$('.resultados').find('.active-filter#'+value).addClass('active');
-			$('.resultados').find('.active-filter#'+value).append('<span class="text">'+option+'</span>');
-			$('.resultados').append($('#'+value));
+		$('.apply-filters-btn').removeAttr('disabled');
+		// modify price filter to new values
+		if ( value === "price" && $resultados.find('.active-filter#price').length > 0 ) {
+			$resultados.find('#price span.text').remove();
+			$resultados.find('#price').append('<span class="text">'+option+'</span>');
+		} else { // create new filter although is price value or not
+			$('#filtersBox .resultados').append('<div class="alert alert-danger alert-dismissible active-filter active" role="alert"><button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">×</span></button></div>')
+			$resultados.find('.active-filter:not([id])').attr('id', value);
+			$resultados.find('.active-filter#'+value).append('<span class="text">'+option+'</span>');
+			$resultados.append($('#'+value));
 		}
-		if ( value == "price" ) {
+		// Add coin symbol
+		if ( value === "price" ) {
 			$('.resultados').find('#price span.text').append('€');
 		}
+		// Add data of filter type
+		$resultados.find('.active-filter#'+value).attr('data-type', type);
 	}
 	
 	/**
@@ -64,12 +70,10 @@ define(['./Base', '../libCommon', 'bootstrap_slider', 'plugins'], function (Base
 	 * @param option:String Selected option to hide
 	 */
 	function disableFilter(option, value) {
-		$('.resultados').find('#'+value).removeClass('active');
-		$('.resultados #'+value+' span.text').remove();
-		$('.resultados').find('#'+value).removeAttr('id');
-		
-		if ( $('.resultados').find('.active-filter.active').length == 0 ) {
+		$('.resultados').find('#'+value).remove();
+		if ( $('.resultados').find('.active-filter').length == 0 ) {
 			$('#filtroInfo').show();
+			$('.apply-filters-btn').attr('disabled', 'disabled');
 		}
 		if ( $('input[value="'+value+'"]').is(':checked') ) {
 			$('input[value="'+value+'"]').attr('checked', false);
@@ -79,6 +83,71 @@ define(['./Base', '../libCommon', 'bootstrap_slider', 'plugins'], function (Base
 		}
 
 		$('.color[value="'+value+'"].selected, .material[value="'+value+'"].selected').removeClass('selected');
+	}
+
+	function applyFilters() {
+		if ( !$(this).is('[disabled=disabled]') ) {
+			// Put filters in url parameters
+			var i = 0;
+			var url = window.location.pathname;
+			var filters = '';
+			$('.resultados .active-filter').each(function() {
+				var type = $(this).attr('data-type');
+				if (type === 'rango') {
+					var option = $(this).find('.text').text();
+					option = option.slice(0,-1);
+					option = option.replace(',','-');
+				} else {
+					var option = $(this).attr('id');
+				}
+				if (i === 0) {
+					url = url + '?' + type + '=' + option;
+				} else {
+					url = url + '&' + type + '=' + option;
+				}
+				i++;
+			});
+			// Put sort order in url parameters
+			var sort = $('#sortList label.active > input').attr('data-url');
+			if ( $('#sortList label.active').is('.change-sort') ) {
+				var asc = true;
+			} else {
+				var asc = false;
+			}
+			url = url + '&orden=' + sort + '&asc=' + asc;
+			// refresh page with parameters
+			window.location.href = url;
+		}
+	}
+
+	function readFilters() {
+		var url = window.location.href;
+		url = url.split('?');
+		url = url[1];
+
+		if ( !url == '' ) {
+			url = url.split('&');
+
+			for ( var i = 0, l = url.length; i < l; i++ ) {
+				var filter = url[i].split('=');
+				if ( filter[0] === 'finish' ) { // it's finish filter
+					$('.filters-section .acabado div[value="'+ filter[1] +'"]').trigger('click');
+				} else if ( filter[0] === 'rango' ) { // it's price filter
+				    var range = filter[1];
+				    range = range.split('-');
+				    $('#priceRange').attr('data-slider-value', '[' + range[0] + ',' + range[1] + ']');
+				} else if ( filter[0] == 'orden' ) { // sort
+					$('#sortList label.active').removeClass('active');
+					$('#sortList label > input[data-url="' + filter[1] + '"]').parent().addClass('active');
+				} else if ( filter[0] == 'asc' ) {
+					if ( filter[1] == 'true' ) {
+						$('#sortList label.active').addClass('change-sort');
+					}
+				} else {
+					$('.filters-columns input[value="' + filter[1] + '"]').trigger( 'click' );
+				}
+			}
+		}
 	}
 
 	/**
@@ -93,7 +162,7 @@ define(['./Base', '../libCommon', 'bootstrap_slider', 'plugins'], function (Base
 		$(this).toggleClass('hide');
 	});
 
-	// Category page - Color selector tooltip filter
+	// Color selector tooltip filter
 	$('.acabado > div').mouseover(function() {
 		var colorName = $(this).attr('value');
 		$(this).tooltip('hide')
@@ -102,7 +171,7 @@ define(['./Base', '../libCommon', 'bootstrap_slider', 'plugins'], function (Base
 			.tooltip('show');
 	});
 
-	// Category page - Toggle button between ASC or DESC in sort list
+	// Toggle button between ASC or DESC in sort list
 	$('#sortList .btn').on('click', function() {
 		if ( $(this).hasClass('active') == true ) {
 			$(this).toggleClass('change-sort');
@@ -111,39 +180,47 @@ define(['./Base', '../libCommon', 'bootstrap_slider', 'plugins'], function (Base
 		}
 	});
 
-	// Category page - Show active filters (checkbox type)
+	// Show active filters (checkbox type)
 	$('#filtersBox input[type="checkbox"]').change(function() {
+	    var type = $(this).closest('.filter-container').find('.filter-label label').attr('data-url');
 	    if(this.checked) {
-	        enableFilter($(this).parent().attr('title'), $(this).val());
+	        enableFilter($(this).parent().attr('title'), $(this).val(), type);
 	    } else {
 	    	disableFilter($(this).parent().attr('title'), $(this).val());
 	    }
 	});
 
-	// Category page - Show active filters (price type)
+	// Show active filters (price type)
 	$('#priceRange').on('slideStop', function() {
 		var value = "price";
-		enableFilter( $(this).attr('value'), value );
+	    var type = $(this).closest('.filter-container').find('.filter-label label').attr('data-url');
+		enableFilter( $(this).attr('value'), value, type);
 	});
 
-	// Category page - Show active filters (color/material type)
+	// Show active filters (color/material type)
 	$('.acabado .color, .acabado .material').on('click', function() {
 		if ( $(this).hasClass('selected') ) {
 			disableFilter( $(this).attr('data-title'), $(this).attr('value') );
 		} else {
-			enableFilter( $(this).attr('data-title'), $(this).attr('value') );
+		    var type = $(this).closest('.filter-container').find('.filter-label label').attr('data-url');
+			enableFilter( $(this).attr('data-title'), $(this).attr('value'), type );
 			$(this).addClass('selected');
 		}
 	});
 
-	// Category page - Disable filter
-	$('.active-filter .close').on('click', function(e) {
+	// Disable filter
+	$('.resultados').on('click', '.active-filter .close', function(e) {
 		e.preventDefault();
 		e.stopPropagation();
 		disableFilter(null, $(this).parent().attr('id'))
 	});
 
-	//Hide lateral contact form -newsletter-
+	// Refresh page with parameters in URL to filter
+	$('.apply-filters-btn').on('click', function() {
+		applyFilters();
+	});
+
+	// Hide lateral contact form -newsletter-
 	$('#SubscribeNewsletterCloseButton, .dark-layer, #alreadySubscribedButton').on('click', function() {
 		$('#subscribeNewsletter').removeClass('show');
 		$('body').removeClass('block-content');
@@ -178,6 +255,9 @@ define(['./Base', '../libCommon', 'bootstrap_slider', 'plugins'], function (Base
     $(document).ready( function() {
 		toggleHeight('seoText', 'hide');
 
+		// Read paramters from url and active current filters
+		readFilters();
+		
 		//Category page - Price range filter with slider
 		$('#priceRange').slider();  
 
