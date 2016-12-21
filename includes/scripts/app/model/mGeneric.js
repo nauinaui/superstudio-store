db2dce7e72761307bc043eaed453a2c09e82ddf2
Arnau Monclús Sánchez <arnaunaui@gmail.com>
2016-12-21 13:29:37 +0100

    added photogallery script to open images in a modal in generic script page for showroom page

--- includes/scripts/app/model/mGeneric.js
+++ includes/scripts/app/model/mGeneric.js
@@ -1,5 +1,6 @@
-define(['./Base.js', 'bootstrap'], function (Base, Bootstrap) {
+define(['./Base.js', '../libCommon.js', 'bootstrap'], function (Base, LibCommon, Bootstrap) {
     var mGeneric = new Base('data for generic pages loaded');
+    var common = new LibCommon();
 
 	/**
 	 * =================
@@ -13,7 +14,32 @@ define(['./Base.js', 'bootstrap'], function (Base, Bootstrap) {
 	 * EVENTS
 	 * =================
 	 */
+	
+	// Show large image when clicking on main image and on secondary images thummbnails if device is not mobile
+	$('.photogallery button[data-target="#largeImageModal"]').on('click', function(e) {
+		console.log('entro');
+		if ( common.detectMobile() == false ) {
+			var source = $(this).find('img').attr('src');
+			console.log(source);
+			source = source.replace('/min/','/max/');
+			source = source.replace('m.jpg','.jpg');
+			$('#largeImageModal .modal-content img').attr('src',source);
+		} else {
+			e.preventDefault();
+			e.stopPropagation();
+			$('#largeImageModal').modal({
+				show: false
+			});
+		}
+	});
 
+	// $('.photogallery a[data-target="#largeImageModal"] img').on('click', function(e) {
+	// 	console.log('entro a imatge');
+	// });
+
+	// $('#largeImageModal').on('shown.bs.modal', function () {
+ //  		console.log('detecto levent a lobrir');
+	// })
 
 	/**
 	 * =================
