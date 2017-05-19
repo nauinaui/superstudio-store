e85d85309f2d3227d410e511299e83a3fe1df300
Arnau Monclús Sánchez <arnaunaui@gmail.com>
2017-05-19 09:53:20 +0200

    PROD VERSION

--- includes/scripts/common.js
+++ includes/scripts/common.js
@@ -7,7 +7,7 @@
 //directory.
 require.config({
     waitSeconds: 20,
-    urlArgs: "v=1.2.0.55",
+    urlArgs: "v=1.2.0.7",
     baseUrl: '/includes/scripts/lib', // /new-portada
     paths: {
       	app: 			  '../app',
